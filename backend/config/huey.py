0e8b690549faf485192d9753b14486ee1cfe7572
deatheragetr <deatheragetr@users.noreply.github.com>
2025-04-23 21:31:32 -0400

    Production docker-compose file works locally to spin up backend api, huey, es, redis, mongo

--- backend/config/huey.py
+++ backend/config/huey.py
@@ -1,7 +1,13 @@
 # config.py
+import os
 from huey import RedisHuey
 
-huey = RedisHuey("explainer-chunk", host="localhost", port=6379, db=0)
+huey = RedisHuey(
+    "explainer-chunk",
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=os.getenv("REDIS_PORT", 6379),
+    db=os.getenv("REDIS_DB", 0),
+)
 # Optional configurations
 huey.immediate = False  # Set to True for development/debug to run tasks immediately
 huey.always_eager = False  # Set to True for testing to run tasks synchronously
